3fdf8a99ec9174e2dc47f2d27f291f8e2ea82039
DanZo0x <dan.cheype@gmail.com>
2023-09-25 16:42:02 +0200

    [ADD] Main & options menu WIP

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    
+
+    public void QuitGame()
+    {
+        Debug.Log("Game quit");
+        Application.Quit();
+    }
+}
