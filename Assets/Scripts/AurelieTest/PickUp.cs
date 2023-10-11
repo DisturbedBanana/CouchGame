9e768d93188d2ee7c1de743adc378466f7ac8d62
vshinori <82277271+vshinori@users.noreply.github.com>
2023-10-11 17:00:19 +0200

    [EDIT] Pick Up

--- Assets/Scripts/AurelieTest/PickUp.cs
+++ Assets/Scripts/AurelieTest/PickUp.cs
@@ -11,13 +11,45 @@ public class PickUp : MonoBehaviour
 
     private GameObject _object;
 
+    private void Update()
+    {
+        if (_isPickingUp)
+        {
+            _pickUpTimer += Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && !_isPickingUp)
+        {
+            _isPickingUp = true;
+            _pickUpTimer = 0f;
+        }
+    }
+
     private void OnTriggerEnter(Collider collision)
     {
-        var input = Input.GetButtonDown("jsp");
-   
         if (collision.CompareTag("RedCube"))
         {
-
+            _object = collision.gameObject;
+            HighlightObject(_object);
         }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.CompareTag("RedCube"))
+        {
+            UnhighlightObject(_object);
+            _object = null;
+        }
+    }
+
+    private void HighlightObject(GameObject gameObject)
+    {
+        gameObject.GetComponent<Outline>().enabled = true;
+    }
+    
+    private void UnhighlightObject(GameObject gameObject)
+    {
+        gameObject.GetComponent<Outline>().enabled = false;
     }
 }
