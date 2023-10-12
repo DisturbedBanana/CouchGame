9bff48c865664d306a125397c77580fcef8e43ac
vshinori <82277271+vshinori@users.noreply.github.com>
2023-10-12 14:15:00 +0200

    [EDIT] Pick Up Finished

--- Assets/Scripts/AurelieTest/PickUp.cs
+++ Assets/Scripts/AurelieTest/PickUp.cs
@@ -4,42 +4,82 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-    [SerializeField] private bool _isPickingUp;
-    [SerializeField] private float _cooldownTime;
+    [SerializeField] private bool _canPickUp;
+    [SerializeField] private float _pickUpCooldown;
+    private float _pickUpCooldownTimer;
     [SerializeField] private float _pickUpTime;
     private float _pickUpTimer;
+    [SerializeField][Range(1, 5)] private float _range;
 
-    private GameObject _object;
+    private GameObject _obj;
+
+    private void Start()
+    {
+        _pickUpCooldownTimer = _pickUpCooldown;
+    }
 
     private void Update()
     {
-        if (_isPickingUp)
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.forward * _range, Color.red);
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, _range))
         {
-            _pickUpTimer += Time.deltaTime;
+            GameObject hitObj = hit.collider.gameObject;
+
+            if (hitObj.CompareTag("RedCube"))
+            {
+                if (_obj != null)
+                    UnhighlightObject(_obj);
+                _obj = hitObj;
+                HighlightObject(_obj);
+            }
+
+            if (Input.GetKeyDown(KeyCode.E) && _canPickUp)
+            {
+                //Debug.Log("start press");
+                _pickUpTimer = 0f;
+            }
+
+            if (Input.GetKey(KeyCode.E) && _canPickUp)
+            {
+                _pickUpTimer += Time.deltaTime;
+
+                if (_pickUpTimer >= _pickUpTime)
+                {
+                    //Debug.Log("end timer");
+                    PickUpObject(hitObj);
+                }
+            }
+
+            if (Input.GetKeyUp(KeyCode.E))
+            {
+                //Debug.Log("released");
+                _pickUpTimer = 0f;
+            }
+
+            if (!_canPickUp)
+                UnhighlightObject(_obj);
+        }
+        else
+        {
+            if (_obj != null)
+                UnhighlightObject(_obj);
+            _obj = null;
         }
 
-        if (Input.GetKeyDown(KeyCode.E) && !_isPickingUp)
+        // Cooldown Pick Up
+        if (!_canPickUp)
         {
-            _isPickingUp = true;
-            _pickUpTimer = 0f;
-        }
-    }
+            //Debug.Log("start cd");
+            _pickUpCooldownTimer -= Time.deltaTime;
 
-    private void OnTriggerEnter(Collider collision)
-    {
-        if (collision.CompareTag("RedCube"))
-        {
-            _object = collision.gameObject;
-            HighlightObject(_object);
-        }
-    }
-
-    private void OnTriggerExit(Collider collision)
-    {
-        if (collision.CompareTag("RedCube"))
-        {
-            UnhighlightObject(_object);
-            _object = null;
+            if (_pickUpCooldownTimer <= 0)
+            {
+                _canPickUp = true;
+                _pickUpCooldownTimer = _pickUpCooldown;
+                //Debug.Log("end cd");
+            }
         }
     }
 
@@ -51,5 +91,13 @@ public class PickUp : MonoBehaviour
     private void UnhighlightObject(GameObject gameObject)
     {
         gameObject.GetComponent<Outline>().enabled = false;
+    }
+
+    private void PickUpObject(GameObject gameObject)
+    {
+        // add item inventaire
+        _canPickUp = false;
+        _pickUpTimer = 0f;
+        //Debug.Log("picked up");
     }
 }
