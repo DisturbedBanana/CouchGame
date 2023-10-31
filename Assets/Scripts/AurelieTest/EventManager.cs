4dc92e3cb9a8b8c9a630b0ef35648ecefe8402be
vshinori <82277271+vshinori@users.noreply.github.com>
2023-10-31 16:05:47 +0100

    [EDIT] EventManager

--- Assets/Scripts/AurelieTest/EventManager.cs
+++ Assets/Scripts/AurelieTest/EventManager.cs
@@ -8,29 +8,46 @@ public class EventManager : MonoBehaviour
 
     [Space]
     [Header("Event Settings")]
-    [Range(60f, 600f)]
+    [Range(1f, 600f)]
     [SerializeField] private float _minInterval;
-    [Range(60f, 600f)]
+    [Range(1f, 600f)]
     [SerializeField] private float _maxInterval;
-    [Range(60f, 600f)]
+    [Range(1f, 600f)]
     [SerializeField] private float _minDuration;
-    [Range(60f, 600f)]
+    [Range(1f, 600f)]
     [SerializeField] private float _maxDuration;
 
     private Event _currentEvent;
+    private float _duration;
 
     private void Start()
     {
-        
+        StartCoroutine(SpawnEvent());
     }
 
-    private void Update()
+    private IEnumerator SpawnEvent()
     {
-        
-    }
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(_minInterval, _maxInterval));
 
-    private void NextEvent()
-    {
+            if (_currentEvent == null)
+            {
+                _currentEvent = _events[Random.Range(0, _events.Length)].GetComponent<Event>();
+                _duration = Random.Range(_minDuration, _maxDuration);
+                Debug.Log($"Starting {_currentEvent.Name} in 10 seconds.");
+                yield return new WaitForSeconds(10f);
+                Debug.Log($"{_currentEvent.Name} started. Duration: {_duration}");
+                Instantiate(_currentEvent, transform.position, Quaternion.identity);
+            }
 
+            yield return new WaitForSeconds(_duration);
+
+            if (_currentEvent != null)
+            {
+                Destroy(_currentEvent);
+                _currentEvent = null;
+            }
+        }
     }
 }
