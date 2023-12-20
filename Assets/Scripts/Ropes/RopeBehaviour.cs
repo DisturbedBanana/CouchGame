aa1c71db88ba77d8f884e2c8d4dd38d5fc64ed8b
DanZo0x <dan.cheype@gmail.com>
2023-12-20 02:17:10 +0100

    [FIX] Props hitboxes & begin ropes

---
+++ Assets/Scripts/Ropes/RopeBehaviour.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RopeBehaviour : MonoBehaviour
+{
+    [Header("References")]
+    public GameObject _ropeDown;
+    public GameObject _ropeUp;
+    public GameObject _rope;
+    private Character _player;
+
+    [Header("Checks")]
+    public bool _ropeIsActivated;
+    public bool _playerIsInZone;
+
+    void Start()
+    {
+        _ropeIsActivated = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _player = other.GetComponent<Character>();
+
+            if (_player.PlayerId == 4)
+            {
+
+            }
+        }
+    }
+
+    public void OnRopActivation(InputAction.CallbackContext context)
+    {
+
+    }
+}
